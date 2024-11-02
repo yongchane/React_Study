a465739c48e974fb5a8924f6a25f57ce1fa96076
yongchane <202021113@sangmyung.kr>
2024-11-02 17:31:45 +0900

    GDG 수업 자료

--- react_study/src/page/index.jsx
+++ react_study/src/page/index.jsx
@@ -5,7 +5,7 @@ const index = () => {
   return (
     <div className="container">
       <div className="title">
-        <div className="main-title">React-Router 수업</div>
+        <div className="main-title">GDG_React-Router 수업</div>
         <div className="exam">
           <Link className="ex1" to="/one">
             예시1
